01cd440fc459e8f65e62fff156b0b5cb996b83bc
vanshika <vanshika@oneplay.in>
2024-01-21 14:22:11 +0530

    Spring Web Application Setup and Controllers Introduction

---
+++ getting-started/src/main/java/com/vanshika/gettingstarted/GettingStartedApplication.java
@@ -0,0 +1,14 @@
+package com.vanshika.gettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GettingStartedApplication.class, args);
+	}
+
+}
